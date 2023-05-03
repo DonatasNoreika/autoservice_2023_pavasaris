f24a22caf06fdafaa36cec0fd4a7df7053e3fd3d
Donatas Noreika <donoras@gmail.com>
2023-05-03 08:32:55 +0300

    6: +Padaryti, kad prie automobilio įrašo galima būtų pridėti nuotrauką ir ji būtų atvaizduojama automobilių puslapyje. Nuotrauka būtų atvaizduojama ir vieno automobilio aprašyme.

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -31,6 +31,7 @@ class Vehicle(models.Model):
     vin = models.CharField(verbose_name="VIN kodas", max_length=17)
     owner_name = models.CharField(verbose_name="Savininkas", max_length=50)
     vehicle_model = models.ForeignKey(to="VehicleModel", verbose_name="Automobilio modelis", on_delete=models.SET_NULL, null=True)
+    photo = models.ImageField(verbose_name="Nuotrauka", upload_to="vehicles", null=True)
 
     def __str__(self):
         return f"{self.vehicle_model} ({self.plate})"
