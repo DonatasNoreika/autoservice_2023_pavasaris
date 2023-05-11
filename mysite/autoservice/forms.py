01ecfd58febf86c9a7505106eb09da185e95a376
Donatas Noreika <donoras@gmail.com>
2023-05-11 08:48:25 +0300

    11: +Padaryti, kuriant užsakymą, datą ir laiką leistų pasirinkti kalendoriuje/laikrodyje (datetimepicker)

--- mysite/autoservice/forms.py
+++ mysite/autoservice/forms.py
@@ -1,6 +1,7 @@
-from .models import OrderComment, Profile
+from .models import OrderComment, Profile, Order
 from django import forms
 from django.contrib.auth.models import User
+
 
 class OrderCommentForm(forms.ModelForm):
     class Meta:
@@ -20,3 +21,14 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['photo']
+
+
+class MyDateTimeInput(forms.DateInput):
+    input_type = 'datetime-local'
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['vehicle', 'deadline', 'status']
+        widgets = {'deadline': MyDateTimeInput()}
